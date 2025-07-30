6044851fe6894b7301c28cd6167d2f60b9d38df2
kagekin <10kgtr@gmail.com>
2025-07-30 23:12:11 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,16 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   base: '/takeakari/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
